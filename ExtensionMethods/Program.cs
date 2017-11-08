010490d84f93c2ed3e2d32518e49e14d4a9bb2fd
Avinash <107341@LTCPU-069-PC>
2017-11-08 17:07:23 +0530

    Added ExtensionMethods

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        // Extension methods are available in a namespace if declared in a static class 
+        // Or imported through using-namespace-directives
+        static void Main(string[] args)
+        {
+            // Understanding ------------
+            // In the Below code we know that Extenstions class is part of the namespace 'ExtensionMethod' so thats the reason its publicly available.
+            string s = "123";
+            int value = s.ToInt32();  // Same as Extensions.ToInt32(s)
+
+            int[] digits = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] a = digits.Slice(4, 3);      // Same as Extensions.Slice(digits, 4, 3)
+
+            X.Test(new A(), new B(), new C());
+
+            Console.ReadLine();
+        }
+    }
+
+    public static class E
+    {
+        public static void F(this object obj, int i)
+        {
+            Console.WriteLine("\r\nClass Called from: " + obj + " Invoking the 1st Extension Method E.F() and Value passed: " + i);
+        }
+        public static void F(this object obj, string s)
+        {
+            Console.WriteLine("\r\nClass Called from: " + obj + " Invoking the 2nd Extension Method E.F()  and Value passed: " + s);
+        }
+    }
+    public class A { }
+    public class B
+    {
+        public void F(int i)
+        {
+            Console.WriteLine("\r\nClass Called by B itself as this method 'F' has taken precedence over the 1st E.F() with value: " + i);
+        }
+    }
+    public class C
+    {
+        public void F(object obj)
+        {
+            Console.WriteLine("\r\nClass Called by C itself as this method 'F' has taken precedence over the 2nd E.F() methods with Type: " + obj.GetType() + " Value: "+ obj);
+        }
+    }
+    public class X
+    {
+        // Same goes with Class E- It has Extension methods "F" and "F" with diff parameters
+        public static void Test(A a, B b, C c)
+        {
+            a.F(1);            // E.F(object, int)
+            a.F("hello");      // E.F(object, string)
+            b.F(1);            // B.F(int)
+            b.F("hello");      // E.F(object, string)
+            c.F(1);            // C.F(object)
+            c.F("hello");      // C.F(object)
+        }
+    }
+}
--- ImplicitlyTypedLocalVariablesProg/Program.cs
+++ ImplicitlyTypedLocalVariablesProg/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,30 @@ namespace ImplicitlyTypedLocalVariablesProg
     {
         static void Main(string[] args)
         {
+            // Implicitly Typed Local Variables are which inferr/conclude that the value assigned to it is of some type.
+            // Not all types can be assigned as explicit types
+
+            //Example:- 'Var'
+
+            //These are the Implicit type variables whom do not assign the explicit types.
+            //These can be well understood when used as Explicit types.
+
+            var i = 5; // We know that var here explicitly is 'int'
+            var s = "Hello"; // var here explicitly is 'String'
+            var d = 1.0; // var here explicitly double
+            var numbers = new int[] { 1, 2, 3 }; // var here explicitly int[]
+            var orders = new Dictionary<int, string>(); // var here explicitly Dictionary<int,string>
+
+            //Lets Get their Types in the output
+
+            Console.WriteLine("var i: " + i.GetType());
+            Console.WriteLine("var s: " + s.GetType());
+            Console.WriteLine("var d: " + d.GetType());
+            Console.WriteLine("var numbers: " + numbers.GetType());
+            
+            Console.WriteLine("var orders: " + orders.GetType());
+
+            Console.ReadKey();
         }
     }
 }
